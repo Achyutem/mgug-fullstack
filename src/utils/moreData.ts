ea43d3a5b70e9b6ca127f313417ce314662b10d8
achyutem <achyutem.ad@gmail.com>
2025-07-19 16:48:30 +0530

    magazine language data

---
+++ src/utils/moreData.ts
@@ -0,0 +1,64 @@
+import type { MagazineTitles } from "./types";
+
+export const arogyaPrabhaTitles: MagazineTitles = {
+  heading: {
+    hindi: "आरोग्य प्रभा",
+    english: "Arogya Prabha",
+  },
+  headers: {
+    title: {
+      hindi: "पत्रिका का शीर्षक",
+      english: "Magazine Title",
+    },
+    date: {
+      hindi: "प्रकाशन तिथि",
+      english: "Publication Date",
+    },
+    link: {
+      hindi: "लिंक",
+      english: "Link",
+    },
+  },
+};
+
+export const sevaPathTitles: MagazineTitles = {
+  heading: {
+    hindi: "सेवा पथ पत्रिका",
+    english: "Seva Path Magazine",
+  },
+  headers: {
+    title: {
+      hindi: "पत्रिका का शीर्षक",
+      english: "Magazine Title",
+    },
+    date: {
+      hindi: "प्रकाशन तिथि",
+      english: "Publication Date",
+    },
+    link: {
+      hindi: "लिंक",
+      english: "Link",
+    },
+  },
+};
+
+export const arogyaPathTitles: MagazineTitles = {
+  heading: {
+    hindi: "आरोग्य पथ",
+    english: "Arogya Path",
+  },
+  headers: {
+    title: {
+      hindi: "पत्रिका का शीर्षक",
+      english: "Magazine Title",
+    },
+    date: {
+      hindi: "प्रकाशन तिथि",
+      english: "Publication Date",
+    },
+    link: {
+      hindi: "लिंक",
+      english: "Link",
+    },
+  },
+};
