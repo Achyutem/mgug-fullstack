4eb60ab00a9735dfd520a53df582c1220a8e9309
achyutem <achyutem.ad@gmail.com>
2025-07-21 16:53:22 +0530

    fetching data types

---
+++ src/utils/urlData.ts
@@ -0,0 +1,22 @@
+import type { MagazineTitles } from "./types";
+
+export const prospectusTitles: MagazineTitles = {
+  heading: {
+    hindi: "विवरणिका",
+    english: "Prospectus",
+  },
+  headers: {
+    title: {
+      hindi: "विवरणिका शीर्षक",
+      english: "Prospectus Title",
+    },
+    date: {
+      hindi: "प्रकाशन तिथि",
+      english: "Date Published",
+    },
+    link: {
+      hindi: "लिंक",
+      english: "Link",
+    },
+  },
+};
