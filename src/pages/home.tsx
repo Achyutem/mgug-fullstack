9697d5e24718057bf1d7f9ffa2a5f6d3de511faa
achyutem <achyutem.ad@gmail.com>
2025-07-30 16:21:27 +0530

    removed navbar and added notification panel

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,4 @@
 import MainLayout from "@/layouts/homeLayout";
-import Navbar from "@/sections/navbar";
 import AboutMain from "@/sections/about";
 import WhyMGUG from "@/sections/whyMGUG";
 import Contact from "@/sections/contact";
@@ -7,18 +6,21 @@ import Main from "@/sections/main";
 import Academic from "@/sections/academic";
 import Admission from "@/sections/admission";
 import { InfoPopup } from "@/components/popup";
+import Notifications from "@/sections/notification";
+// import Marquee from "@/sections/marquee";
 
 const UniversityLandingPage: React.FC = () => {
   return (
     <div className=" min-h-screen bg-black text-white overflow-hidden">
       <MainLayout>
         <InfoPopup />
-        <Navbar />
+        {/* <Marquee /> */}
         <Main />
         <AboutMain />
         <WhyMGUG />
         <Academic />
         <Admission />
+        <Notifications />
         <Contact />
       </MainLayout>
     </div>
