33f7d611dd0bd217666566c306fa314638941e52
achyutem <achyutem.ad@gmail.com>
2025-08-22 15:15:21 +0530

    changed border

--- src/components/newCard.tsx
+++ src/components/newCard.tsx
@@ -4,7 +4,7 @@ const NewCard = ({ icon, title, href }: NewCardProps) => {
   return (
     <a
       href={href}
-      className="group relative bg-orange-500/10 backdrop-blur-lg border border-orange-600 rounded-2xl p-6 text-center shadow-sm transition-all duration-300 ease-in-out hover:shadow-orange-500/20 hover:border-orange-400 hover:-translate-y-2 flex flex-col items-center justify-center"
+      className="group relative bg-orange-500/10 backdrop-blur-lg border border-orange-600 rounded-2xl p-6 text-center shadow-sm transition-all duration-300 ease-in-out hover:shadow-orange-500/20 hover:scale-105 hover:border-orange-500 hover:-translate-y-2 flex flex-col items-center justify-center"
     >
       <div className="text-4xl text-orange-600 transition-colors duration-300 group-hover:text-orange-500">
         {icon}
